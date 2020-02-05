c9ecf1886f88ea183a71e7df25ae607360391c82
Marina Agalakova <agalakova.m.a@gmail.com>
2020-02-05 23:42:56 +0300

    pass events and delete element without mutating state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,22 +7,35 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', id: 1 },
-    { label: 'Build React App', important: true, id: 2 },
-    { label: 'Have a lunch', id: 3 },
-  ];
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', id: 1 },
+      { label: 'Build React App', important: true, id: 2 },
+      { label: 'Have a lunch', id: 3 },
+    ],
+  };
 
-export default App;
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
+      const result = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: result,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+      </div>
+    );
+  }
+}
