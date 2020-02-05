8d4ba43f1247d4a991f87d58f4142801b8c61c71
Marina Agalakova <agalakova.m.a@gmail.com>
2020-02-05 12:36:45 +0300

    change commit email address

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -24,14 +24,12 @@ export default class TodoListItem extends Component {
         >
           {label}
         </span>
-
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
         >
           <i className="fa fa-exclamation" />
         </button>
-
         <button
           type="button"
           className="btn btn-outline-danger btn-sm float-right"
