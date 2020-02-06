55aa7e27b49bd2dd2fed2143ea3bc295c46c2ef8
Marina Agalakova <agalakova.m.a@gmail.com>
2020-02-06 23:02:29 +0300

    move all data to parent component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,7 @@ import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import ItemAddForm from '../item-add-form';
 
+import 'normalize.css';
 import './app.css';
 
 export default class App extends Component {
@@ -44,16 +45,31 @@ export default class App extends Component {
     });
   };
 
+  onToggleDone = id => {
+    console.log('Done', id);
+  };
+
+  onToggleImportant = id => {
+    console.log('Important', id);
+  };
+
   render() {
     return (
-      <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
+      <div className="todo-app-wrapper">
+        <div className="todo-app">
+          <AppHeader toDo={1} done={3} />
+          <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+          </div>
+          <TodoList
+            todos={this.state.todoData}
+            onDeleted={this.deleteItem}
+            onToggleDone={this.onToggleDone}
+            onToggleImportant={this.onToggleImportant}
+          />
+          <ItemAddForm onItemAdded={this.addItem} />
         </div>
-        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
-        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,13 +2,18 @@ import React from 'react';
 import TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant }) => {
   const elements = todos.map(item => {
     const { id, ...itemProps } = item;
 
     return (
       <li className="list-group-item" key={id}>
-        <TodoListItem {...itemProps} onDeleted={() => onDeleted(id)} />
+        <TodoListItem
+          {...itemProps}
+          onDeleted={() => onDeleted(id)}
+          onToggleDone={() => onToggleDone(id)}
+          onToggleImportant={() => onToggleImportant(id)}
+        />
       </li>
     );
   });
